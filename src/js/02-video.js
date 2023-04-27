1e503f4df696d00dd287f1a03f28d48d7f406de0
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-04-27 14:24:56 +0200

    feature/task o2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from "@vimeo/player/src/player";
+import throttle from "lodash.throttle";
+
+const player = new Player('vimeo-player');
+
+const saveTimeToLocalStorage = localStorage.setItem('videoplayer-current-time', 'time');
+
+player.on('timeupdate', saveTimeToLocalStorage);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(currentTime);
 
